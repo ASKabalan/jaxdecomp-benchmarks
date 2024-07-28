8ab32c888cae360e03dc0bbb20c997d313c5a06e
Wassim KABALAN <wastondev@gmail.com>
2024-07-28 12:54:14 +0200

    2048 plots

--- plot_all.sh
+++ plot_all.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 gpus="4 8 16 32 64"
-sizes="1024"
+sizes="1024 2048"
 time_aggregations="mean"
 time_columns="mean_time"
-figure_size="20 10"
+figure_size="15 7"
 precisions=("float32" "float64")
 scaling=("Weak" "Strong")
 fft_types=("FFT" "IFFT")
@@ -20,38 +20,40 @@ jax_v100_new="slabs_new/v100/JAX.csv"
 jax_v100_old="slabs_old/v100/old_JAX.csv"
 
 
+for size in ${sizes[@]}; do
 
-# compare new JAXDECOMP with new JAX
-for precision in ${precisions[@]}; do
-    for fft_type in ${fft_types[@]}; do
-        hpc-plotter plot -f $jaxdecomp_a100_new $jax_a100_new -fs $figure_size -g $gpus -ta $time_aggregations -p $precision -fn $fft_type -d $sizes -db $dark_mode -o plots/gpus_a100_NN_${fft_type}_${precision}.png -sc Strong -tc $time_columns -ps plot_all -b $backend
-    done
-done
+  # compare new JAXDECOMP with new JAX
+  for precision in ${precisions[@]}; do
+      for fft_type in ${fft_types[@]}; do
+          hpc-plotter plot -f $jaxdecomp_a100_new $jax_a100_new -fs $figure_size -g $gpus -ta $time_aggregations -p $precision -fn $fft_type -d $size -db $dark_mode -o plots/gpus_a100_NN_${fft_type}_${precision}_${size}.png -sc Strong -tc $time_columns -ps plot_all -b $backend
+      done
+  done
 
-# compare new JAXDECOMP with new JAX V100
-for precision in ${precisions[@]}; do
-    for fft_type in ${fft_types[@]}; do
-        hpc-plotter plot -f $jaxdecomp_v100_new $jax_v100_new -fs $figure_size -g $gpus -ta $time_aggregations -p $precision -fn $fft_type -d $sizes -db $dark_mode -o plots/gpus_v100_NN_${fft_type}_${precision}.png -sc Strong -tc $time_columns -ps plot_all -b $backend
-    done
-done
+  # compare new JAXDECOMP with new JAX V100
+  for precision in ${precisions[@]}; do
+      for fft_type in ${fft_types[@]}; do
+          hpc-plotter plot -f $jaxdecomp_v100_new $jax_v100_new -fs $figure_size -g $gpus -ta $time_aggregations -p $precision -fn $fft_type -d $size -db $dark_mode -o plots/gpus_v100_NN_${fft_type}_${precision}_${size}.png -sc Strong -tc $time_columns -ps plot_all -b $backend
+      done
+  done
 
-# compare old JAXDECOMP with old JAXDECOMP a100
-for precision in ${precisions[@]}; do
-    for fft_type in ${fft_types[@]}; do
-        hpc-plotter plot -f $jaxdecomp_a100_old $jax_a100_old -fs $figure_size -g $gpus -ta $time_aggregations -p $precision -fn $fft_type -d $sizes -db $dark_mode -o plots/gpus_a100_NO_${fft_type}_${precision}.png -sc Strong -tc $time_columns -ps plot_all -b $backend
-    done
-done
+  # compare old JAXDECOMP with old JAXDECOMP a100
+  for precision in ${precisions[@]}; do
+      for fft_type in ${fft_types[@]}; do
+          hpc-plotter plot -f $jaxdecomp_a100_old $jax_a100_old -fs $figure_size -g $gpus -ta $time_aggregations -p $precision -fn $fft_type -d $size -db $dark_mode -o plots/gpus_a100_NO_${fft_type}_${precision}_${size}.png -sc Strong -tc $time_columns -ps plot_all -b $backend
+      done
+  done
 
-# compare old JAX with old JAX jax_a100
-for precision in ${precisions[@]}; do
-    for fft_type in ${fft_types[@]}; do
-        hpc-plotter plot -f $jaxdecomp_v100_old $jax_v100_old -fs $figure_size -g $gpus -ta $time_aggregations -p $precision -fn $fft_type -d $sizes -db $dark_mode -o plots/gpus_v100_JNO_${fft_type}_${precision}.png -sc Strong -tc $time_columns -ps plot_all -b $backend
-    done
-done
+  # compare old JAX with old JAX jax_a100
+  for precision in ${precisions[@]}; do
+      for fft_type in ${fft_types[@]}; do
+          hpc-plotter plot -f $jaxdecomp_v100_old $jax_v100_old -fs $figure_size -g $gpus -ta $time_aggregations -p $precision -fn $fft_type -d $size -db $dark_mode -o plots/gpus_v100_JNO_${fft_type}_${precision}_${size}.png -sc Strong -tc $time_columns -ps plot_all -b $backend
+      done
+  done
 
-# compare old JAXDECOMP with new JAXDECOMP v100
-for precision in ${precisions[@]}; do
-    for fft_type in ${fft_types[@]}; do
-        hpc-plotter plot -f $jaxdecomp_v100_old $jaxdecomp_v100_new -fs $figure_size -g $gpus -ta $time_aggregations -p $precision -fn $fft_type -d $sizes -db $dark_mode -o plots/gpus_v100_NO_${fft_type}_${precision}.png -sc Strong -tc $time_columns -ps plot_all -b $backend
-    done
+  # compare old JAXDECOMP with new JAXDECOMP v100
+  for precision in ${precisions[@]}; do
+      for fft_type in ${fft_types[@]}; do
+          hpc-plotter plot -f $jaxdecomp_v100_old $jaxdecomp_v100_new -fs $figure_size -g $gpus -ta $time_aggregations -p $precision -fn $fft_type -d $size -db $dark_mode -o plots/gpus_v100_NO_${fft_type}_${precision}_${size}.png -sc Strong -tc $time_columns -ps plot_all -b $backend
+      done
+  done
 done
